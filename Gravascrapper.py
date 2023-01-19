3ff882dc050f155bd8ee834e5e37c568268e5d1c
Shojun <113646001+Shojunn@users.noreply.github.com>
2023-01-19 22:28:48 +0000

    Add files via upload

--- Gravascrapper.py
+++ Gravascrapper.py
@@ -2,34 +2,76 @@ import requests
 import sys
 from art import tprint
 
-#banner
+##banner
 tprint("GRAVASCRAPPER")
 print("Gravatar passive data collection tool by Shojunn")
 print()
 
-#argument
+##argument
 handle = sys.argv[1]
 print("=====INPUT=====")
-print("The input received was " + handle)
+print()
+print("The input received was '" + handle + "'.")
 print()
 url = "https://www.gravatar.com/" + handle + ".json"
 jasao = requests.get(url).json()
 
-#raw output
+"""
+##raw output
 print("=====OUTPUT=====")
+print()
 print(jasao)
 print()
+"""
 
-#refined output
+##refined output
 
-##ALTERNATIVE
 print("=====OUTPUT=====")
+print()
+
+#id
+if not jasao["entry"][0]["id"] != 0:
+    print("ID: this field was left empty")
+else:
+    print("ID: " + jasao["entry"][0]["id"])
+
+#username
 if not jasao["entry"][0]["preferredUsername"] != 0:
-    print("USERNAME: this field is empty")
+    print("USERNAME: this field was left empty")
 else:
     print("USERNAME: " + jasao["entry"][0]["preferredUsername"])
 
-##TRY-EXCEPT
+#name
+if not jasao["entry"][0]["name"] != 0:
+    print("NAME: this field was left empty")
+else:
+    print("NAME: " + str(jasao["entry"][0]["name"]["formatted"]))
+
+#aboutme
+if not jasao["entry"][0]["aboutMe"] != 0:
+    print("BIO: this field was left empty")
+else:
+    print("BIO: " + jasao["entry"][0]["aboutMe"])
+
+#urls
+if len(jasao["entry"][0]["urls"]) <= 3:
+    print("URL: this field was left empty")
+else:
+    print("URL (OPEN WITH CARE): " + str(jasao["entry"][0]["urls"]))
+
+#thumbnailUrl
+if not jasao["entry"][0]["thumbnailUrl"] != 0:
+    print("Thumbnail: this field was left empty")
+else:
+    print("Thumbnail: " + jasao["entry"][0]["thumbnailUrl"])
+
+print()
+print("=====FINISHED=====")
+print()
+
+
+
+"""
 try:
     print("USERNAME: " + jasao["entry"][0]["preferredUsername"])
 except:
@@ -37,3 +79,4 @@ except:
 
 #type check
 print(type(jasao["entry"]))
+"""
